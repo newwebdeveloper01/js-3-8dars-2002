7c0d982a913b237863aff5209ce26574affe034e
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2022-08-25 01:17:55 +0500

    misol oson ekan

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var masofa = document.querySelector(".masofa");
+var piyoda = document.querySelector(".piyoda");
+var samalyot = document.querySelector(".samalyot");
+var velosipedchi = document.querySelector(".velosipedchi");
+var mashina = document.querySelector(".mashina");
+var button = document.querySelector(".button");
+
+
+
+
+
+button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    const content = {
+        s: masofa.value-0
+    }
+        // let a = (content.s/800).toFixed(1)
+        // let c=(a-0).toFixed(0)
+        // let x=(a*10%10);
+        // let h=(x-0).toFixed(0)
+        // console.log(x)
+    function tekshirish(masofa) {
+        if (typeof content.s !== 'number' && isNaN(content.s - 0))
+            return false;
+        return true;
+
+    }
+
+    function hisob(b) {
+        let a = (content.s/b).toFixed(1)
+        let c=(a-0).toFixed(0)
+        let x=(a*10%10)*6;
+        let h=(x-0).toFixed(0)
+        return c+" soat "+h+" min";
+
+    }
+
+    var p = hisob(3.6)
+    piyoda.textContent = p + " ";
+    
+    
+    var o = hisob(70)
+    mashina.textContent = o + " ";
+
+    var l = hisob(20.1)
+    velosipedchi.textContent = l + " ";
+
+    var j = hisob(800)
+    samalyot.textContent = j + " ";
+
+
+})
